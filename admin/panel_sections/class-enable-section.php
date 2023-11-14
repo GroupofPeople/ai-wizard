88f4f3957b2021866eaf8b9491ac6ea2c44bcca9
Alexander-Wolf-GofP <alexander.wolf@groupofpeople.de>
2023-11-14 15:42:05 +0100

    Updated all escapeable echos

--- admin/panel_sections/class-enable-section.php
+++ admin/panel_sections/class-enable-section.php
@@ -9,16 +9,16 @@ class Enable_Section extends Panel_Section {
 	public function render_section( $ai_wizard_form, $post ) {
 		?>
         <fieldset class="enable-container <?php echo AI_Wizard_Form::once_enabled() && !$ai_wizard_form->is_enabled() ? "enable-container-disabled" : ""; ?>">
-            <label for="<?php echo self::PREFIX; ?>-active"><?php echo esc_html( __( "Enable ChatGPT extension for this form", 'ai-wizard' ) ); ?></label>
+            <label for="<?php echo esc_html(self::PREFIX); ?>-active"><?php echo esc_html__( "Enable ChatGPT extension for this form", 'ai-wizard' ); ?></label>
             <label class="switch">
-                <input type="checkbox" name="<?php echo self::PREFIX; ?>[active]"
-                       id="<?php echo self::PREFIX; ?>-active" <?php echo $ai_wizard_form->is_enabled() ? "checked" : ""; ?>
-	                <?php echo AI_Wizard_Form::once_enabled() && !$ai_wizard_form->is_enabled() ? "disabled" : ""; ?>>
+                <input type="checkbox" name="<?php echo esc_html(self::PREFIX); ?>[active]"
+                       id="<?php echo esc_html(self::PREFIX); ?>-active" <?php echo esc_html($ai_wizard_form->is_enabled() ? "checked" : ""); ?>
+	                <?php echo esc_html(AI_Wizard_Form::once_enabled() && !$ai_wizard_form->is_enabled() ? "disabled" : ""); ?>>
                 <span class="slider round"></span>
             </label>
             <?php
             if(AI_Wizard_Form::once_enabled() && !$ai_wizard_form->is_enabled()){
-                $this->tool_tip(__('Please note that AI Wizard can only be used for one CF7 form in total on your WordPress site. Visit the developer page to receive more information.', 'ai-wizard'));
+                $this->tool_tip(esc_html__('Please note that AI Wizard can only be used for one CF7 form in total on your WordPress site. Visit the developer page to receive more information.', 'ai-wizard'));
             }
             ?>
         </fieldset>
